c70dbf30b5034a7e7f714a02fcf1bec8ce9ada6b
YOHANNES-DEGEFA <138358695+YOHANNES-DEGEFA@users.noreply.github.com>
2023-12-21 06:56:10 -0800

    Added solution for Count Equal and Divisible Pairs in an Array

--- week1/count-equal-and-divisible-pairs-in-an-array.py
+++ week1/count-equal-and-divisible-pairs-in-an-array.py
@@ -1,13 +1,41 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
+        hashMap = {}
+        result = 0 
 
-        answer = 0 
-        for i in range(len(nums)):
-            
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j] and (i*j)%k == 0:
-                    answer += 1 
+        #Block 1: map numbers with their indices
+        for i, num in enumerate(nums):
+            if num in hashMap: 
+                hashMap[num].append(i)
+            else: 
+                hashMap[num] = [i]
 
-        return answer
+        #Block 2: function to calculate the number of indices that
+        # are divisisible by k  
+
+        def calcIdxProdDivsblebyK(indices):
+            n = len(indices)
+            count = 0 
+
+            # n-1 cause there is no element next to pairs[n-1] element
+            for i in range(n-1): 
+                for j in range(i+1,n):
+                    if (indices[i]*indices[j]) %k == 0: 
+                        count += 1 
+
+            return count 
+
+        
+        #Block 3: One more traversal to find our result 
+        nums = list(set(nums))
+        for num in nums: 
+            if len(hashMap[num]) > 1: 
+                result += calcIdxProdDivsblebyK(hashMap[num])
+
+        return result 
+
+
+
+      
 
         
