37d5d38444fb56df5435454d58f4cc355c12de0f
YOHANNES-DEGEFA <138358695+YOHANNES-DEGEFA@users.noreply.github.com>
2023-12-22 14:02:43 -0800

    Added solution for Rotate Image

---
+++ week1/rotate-image.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i,n):
+                matrix[i][j],matrix[j][i]= matrix[j][i],matrix[i][j]
+
+        for row in matrix:
+            l, r = 0, n-1
+            while l < r:
+                row[l],  row[r] = row[r], row[l]
+                l += 1 
+                r -= 1 
+
+        return matrix
+
+            
+
+
+        
