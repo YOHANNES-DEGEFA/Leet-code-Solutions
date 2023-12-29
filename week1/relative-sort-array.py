e2a73fc49a89a8c4bfb028cb973024096b32710e
YOHANNES-DEGEFA <138358695+YOHANNES-DEGEFA@users.noreply.github.com>
2023-12-29 07:15:32 -0800

    Added solution for Relative Sort Array

--- week1/relative-sort-array.py
+++ week1/relative-sort-array.py
@@ -1,17 +1,16 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        arr2ToIdx = {}
+        elem = set(arr2)
 
-        idx_dict = {}
-        for i in range(len(arr2)):
-            idx_dict[arr2[i]]  = i 
-
-        arr2 = set(arr2)
         N = len(arr1)
-        def comp_by_idx(num):
-            if num not in arr2: 
-                return N + num 
+        for i, n in enumerate(arr2): 
+            arr2ToIdx[n] = i 
 
-            return idx_dict[num]
+        def comparator(n):
+            if n not in elem: 
+                return n + N 
+            return arr2ToIdx[n]
 
-        arr1.sort(key=comp_by_idx)
+        arr1.sort(key=comparator)
         return arr1
