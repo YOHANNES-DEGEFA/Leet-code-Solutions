dc81852ca6465506f0100f80b00045c63546f9ff
Yohannes Degefa <138358695+YOHANNES-DEGEFA@users.noreply.github.com>
2025-01-02 19:41:29 +0300

    Included a comment that clarify what the alogrithm does

--- "C:\Users\Yohannnes\Desktop\A2Sv companion"/leetcode/running-sum-of-1d-array.py
+++ "C:\Users\Yohannnes\Desktop\A2Sv companion"/leetcode/running-sum-of-1d-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        
+        # just return the prefix sum of the array 
         for i in range(1,len(nums)): 
             nums[i] += nums[i-1]
             
